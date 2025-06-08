31a2721a5afd6c7848b0e7c8125902e2e97442c4
Number_Sir <number_sir@126.com>
2025-06-09 01:55:56 +0800

    feat(toast): add toast for notification
    currently only working on win10/win11

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
-from src import Project, settings
+import time
+
+from src.config import settings
+from src.core import Project
+from src.log import logger
+from src.toast import Toaster
 
 
 def main():
+    start = time.time()
     project = Project()
     project.clean(settings.filepath.root / settings.filepath.tmp)
     project.converter.convert()
+    # project.restorer.restore()
+    end = time.time()
+    return end - start
 
 
 if __name__ == '__main__':
-    main()
+    last = main()
+    logger.info(f"===== Lasting {last or -1:.2f}s =====")
+
+    Toaster(
+        title="农场汉化脚本",
+        body=(
+            "农场汉化脚本运行完啦"
+            "\n"
+            f"耗时 {last or -1:.2f}s"
+        ),
+        logo=settings.filepath.resource / "img" / "icon.png"
+    ).notify()
--- src/__init__.py
+++ src/__init__.py
@@ -6,3 +6,4 @@ from .exception import *
 from .log import *
 from .model import *
 from .paratranz import *
+from .toast import Toaster
